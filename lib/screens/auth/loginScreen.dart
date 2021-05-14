126d8ea94c55a1b18289df6e348c00d5c91791a2
Rojan Shrestha <roj.shr1996@gmail.com>
2021-05-14 09:41:20 +0545

    platform channel for getting battery level added

--- lib/screens/auth/loginScreen.dart
+++ lib/screens/auth/loginScreen.dart
@@ -28,178 +28,188 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-      child: Scaffold(
-        backgroundColor: Colors.blueGrey[900],
-        body: Container(
-          height: Utilities.screenHeight(context),
-          width: Utilities.screenWidth(context),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(height: 30),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 12, right: 12),
-                          child: Container(
-                            child: TextFormField(
-                              controller: emailController,
-                              keyboardType: TextInputType.emailAddress,
-                              validator: (String? value) {
-                                if (value!.isEmpty) {
-                                  return "Email field cannot be empty";
-                                }
-                                String p =
-                                    r"^([a-zA-Z0-9_\-\+\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]+)$";
-                                RegExp regExp = new RegExp(p);
-                                if (!regExp.hasMatch(value)) {
-                                  return "Enter a valid email";
-                                }
-                                return null;
-                              },
-                              style: TextStyle(
-                                  fontSize: 16, color: AppColor.primary),
-                              decoration: const InputDecoration(
-                                hintText: 'What do people call you?',
-                                labelStyle: TextStyle(
-                                  color: AppColor.white,
+      child: Container(
+        color: Colors.blueGrey[900],
+        child: SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.blueGrey[900],
+            body: Container(
+              height: Utilities.screenHeight(context),
+              width: Utilities.screenWidth(context),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(height: 30),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 12, right: 12),
+                              child: Container(
+                                child: TextFormField(
+                                  controller: emailController,
+                                  keyboardType: TextInputType.emailAddress,
+                                  validator: (String? value) {
+                                    if (value!.isEmpty) {
+                                      return "Email field cannot be empty";
+                                    }
+                                    String p =
+                                        r"^([a-zA-Z0-9_\-\+\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]+)$";
+                                    RegExp regExp = new RegExp(p);
+                                    if (!regExp.hasMatch(value)) {
+                                      return "Enter a valid email";
+                                    }
+                                    return null;
+                                  },
+                                  style: TextStyle(
+                                      fontSize: 16, color: AppColor.primary),
+                                  decoration: const InputDecoration(
+                                    hintText: 'What do people call you?',
+                                    labelStyle: TextStyle(
+                                      color: AppColor.white,
+                                    ),
+                                    hintStyle: TextStyle(
+                                      color: AppColor.muted,
+                                    ),
+                                    labelText: 'Email',
+                                    focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: AppColor.primary)),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: AppColor.primary)),
+                                    disabledBorder: OutlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: AppColor.muted)),
+                                    border: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: AppColor.primary)),
+                                    errorBorder: OutlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: AppColor.danger)),
+                                  ),
                                 ),
-                                hintStyle: TextStyle(
-                                  color: AppColor.muted,
-                                ),
-                                labelText: 'Email',
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: AppColor.primary)),
-                                enabledBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: AppColor.primary)),
-                                disabledBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: AppColor.muted)),
-                                border: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: AppColor.primary)),
-                                errorBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: AppColor.danger)),
                               ),
                             ),
-                          ),
-                        ),
-                        SizedBox(height: 12),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 12, right: 12),
-                          child: Container(
-                            child: TextFormField(
-                              controller: passwordController,
-                              keyboardType: TextInputType.visiblePassword,
-                              obscureText: true,
-                              validator: (String? value) {
-                                if (value!.isEmpty) {
-                                  return "Password field cannot be empty";
-                                }
-                                return null;
-                              },
-                              style: TextStyle(
-                                  fontSize: 16, color: AppColor.primary),
-                              decoration: const InputDecoration(
-                                hintText: 'What do people call you?',
-                                labelStyle: TextStyle(
-                                  color: AppColor.white,
+                            SizedBox(height: 12),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 12, right: 12),
+                              child: Container(
+                                child: TextFormField(
+                                  controller: passwordController,
+                                  keyboardType: TextInputType.visiblePassword,
+                                  obscureText: true,
+                                  validator: (String? value) {
+                                    if (value!.isEmpty) {
+                                      return "Password field cannot be empty";
+                                    }
+                                    return null;
+                                  },
+                                  style: TextStyle(
+                                      fontSize: 16, color: AppColor.primary),
+                                  decoration: const InputDecoration(
+                                    hintText: 'What do people call you?',
+                                    labelStyle: TextStyle(
+                                      color: AppColor.white,
+                                    ),
+                                    hintStyle: TextStyle(
+                                      color: AppColor.muted,
+                                    ),
+                                    labelText: 'Password',
+                                    focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: AppColor.primary)),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: AppColor.primary)),
+                                    disabledBorder: OutlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: AppColor.muted)),
+                                    border: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: AppColor.primary)),
+                                    errorBorder: OutlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: AppColor.danger)),
+                                  ),
                                 ),
-                                hintStyle: TextStyle(
-                                  color: AppColor.muted,
-                                ),
-                                labelText: 'Password',
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: AppColor.primary)),
-                                enabledBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: AppColor.primary)),
-                                disabledBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: AppColor.muted)),
-                                border: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: AppColor.primary)),
-                                errorBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: AppColor.danger)),
                               ),
                             ),
-                          ),
-                        ),
-                        SizedBox(height: 30),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 12, right: 12),
-                          child: Container(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Expanded(
-                                  child: Material(
-                                    elevation: 2,
-                                    color: AppColor.danger,
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(5)),
-                                    child: InkWell(
-                                      highlightColor: Colors.transparent,
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(5)),
-                                      onTap: () {
-                                        validator();
-                                        // Utilities.openActivity(
-                                        //     context, HomeScreen());
-                                      },
-                                      child: Container(
-                                        height: 48,
-                                        child: Center(
-                                          child: Text(
-                                            "Login".toUpperCase(),
-                                            style: TextStyle(
-                                              color: AppColor.white,
+                            SizedBox(height: 30),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 12, right: 12),
+                              child: Container(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Material(
+                                        elevation: 2,
+                                        color: AppColor.danger,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(5)),
+                                        child: InkWell(
+                                          highlightColor: Colors.transparent,
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(5)),
+                                          onTap: () {
+                                            validator();
+                                            // Utilities.openActivity(
+                                            //     context, HomeScreen());
+                                          },
+                                          child: Container(
+                                            height: 48,
+                                            child: Center(
+                                              child: Text(
+                                                "Login".toUpperCase(),
+                                                style: TextStyle(
+                                                  color: AppColor.white,
+                                                ),
+                                              ),
                                             ),
                                           ),
                                         ),
                                       ),
                                     ),
-                                  ),
+                                  ],
                                 ),
-                              ],
+                              ),
                             ),
-                          ),
-                        ),
-                        SizedBox(height: 25),
-                        Align(
-                          alignment: Alignment.center,
-                          child: TextButton(
-                            onPressed: () {
-                              final form = _formKey.currentState;
-                              form!.reset();
-                              Utilities.openActivity(context, SignupScreen());
-                            },
-                            child: Text(
-                              "NOT A USER? SIGN UP",
-                              style: TextStyle(
-                                  color: AppColor.white, fontSize: 16),
+                            SizedBox(height: 25),
+                            Align(
+                              alignment: Alignment.center,
+                              child: TextButton(
+                                onPressed: () {
+                                  final form = _formKey.currentState;
+                                  form!.reset();
+                                  Utilities.openActivity(
+                                      context, SignupScreen());
+                                },
+                                child: Text(
+                                  "NOT A USER? SIGN UP",
+                                  style: TextStyle(
+                                      color: AppColor.white, fontSize: 16),
+                                ),
+                              ),
                             ),
-                          ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
--- lib/screens/home/homeScreen.dart
+++ lib/screens/home/homeScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:phone_auth_test/main.dart';
 import 'package:phone_auth_test/screens/auth/loginScreen.dart';
 import 'package:phone_auth_test/services/authenticationServices.dart';
@@ -12,6 +15,24 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final AuthenticationServices _auth = AuthenticationServices();
 
+  String _batteryLevel = "Unknown";
+
+  static const platform = const MethodChannel('samples.flutter.dev/battery');
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod("getBatteryLevel");
+      batteryLevel = "Battery level at $result %";
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,13 +45,6 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         automaticallyImplyLeading: false,
         actions: [
-          // IconButton(
-          //     icon: Icon(Icons.logout),
-          //     color: AppColor.primary,
-          //     onPressed: () {
-          //       print("log out");
-          //     })
-
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: ElevatedButton(
@@ -43,11 +57,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   }
                 });
               },
-              // style:
-              // ElevatedButton.styleFrom(
-              //   primary: AppColor.primaryDark, // background
-              //   onPrimary: Colors.white, // foreground
-              // ),
               style: ButtonStyle(
                   overlayColor: MaterialStateProperty.resolveWith<Color>(
                     (Set<MaterialState> states) {
@@ -66,6 +75,55 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ],
+      ),
+      body: Container(
+        height: Utilities.screenHeight(context),
+        // width: Utilities.screenWidth(context),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Container(
+                child: Text(
+                  "$_batteryLevel",
+                  style: TextStyle(color: AppColor.white, fontSize: 16),
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  child: ElevatedButton(
+                    onPressed: _getBatteryLevel,
+                    style: ButtonStyle(
+                      overlayColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered))
+                            return AppColor.primaryDarkAccent;
+                          if (states.contains(MaterialState.focused) ||
+                              states.contains(MaterialState.pressed))
+                            return AppColor.primaryDarkAccent;
+                          return AppColor
+                              .primaryDarkAccent; // Defer to the widget's default.
+                        },
+                      ),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          AppColor.primaryDark),
+                    ),
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text("Get Battery"),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
